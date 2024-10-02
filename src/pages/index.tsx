10e01027c828e24fa86f2247f581a85e23b072a2
Adam Lear <adamlear12@gmail.com>
2024-10-01 22:40:44 -0500

    restyle and convert to gatsby

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,103 +1,85 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+import type { HeadFC, PageProps } from "gatsby";
+import * as React from "react";
+import { AboutMe } from "../components/AboutMe";
+import { ExperienceSection } from "../components/Experience";
+import { Experience } from "../types/types";
 
 const IndexPage: React.FC<PageProps> = () => {
-  return (
-    <main>
+    const education: Experience[] = [
+        {
+            location: "Drake University",
+            position: null,
+            startMonth: "August 2024",
+            endMonth: "May 2024",
+            description: [
+                "Majors: Computer Science & Artificial Intelligence",
+                "GPA: 3.88",
+                "Favorite Courses: Software Engineering, Algorithm Analysis, Programming Languages",
+            ],
+        },
+    ];
 
-    </main>
-  )
-}
+    const work: Experience[] = [
+        {
+            location: "Tractor Zoom",
+            position: "Software Engineer",
+            startMonth: "June 2024",
+            endMonth: "Present",
+            description: [],
+        },
+        {
+            location: "Hy-Vee, Inc.",
+            position: "Data Science Intern",
+            startMonth: "June 2023",
+            endMonth: "August 2023",
+            description: [
+                "Use Python and SQL to explore and transform data",
+                "Create code for the gathering, merging, and aggregating of data",
+                "Utilize Artificial Intelligence and other Natural Language Processing techniques to predict item labels",
+                "Wrote unit tests for a large classication prediction model",
+            ],
+        },
+        {
+            location: "Federal Reserve Bank of Chicago",
+            position: "Software Engineering Intern",
+            startMonth: "June 2022",
+            endMonth: "August 2022",
+            description: [
+                "Developed, tested, and deployed code for application that processes payments for 10,000+ organizations",
+                "Collaborated with Scrum team using Agile methodologies",
+                "Improved security and performance public-facing webpages",
+                "Modified web applications using the Angular framework",
+            ],
+        },
+    ];
 
-export default IndexPage
+    return (
+        <main className="px-4 py-16 sm:px-6 md:px-8 min-h-screen flex justify-center">
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+                <section>
+                    <AboutMe />
+                </section>
+                <section className="flex flex-col gap-3">
+                    <h1 className="text-2xl font-semibold">Education</h1>
+                    {education.map((value, index) => (
+                        <div key={index}>
+                            <ExperienceSection experience={value} />
+                        </div>
+                    ))}
+                </section>
+                <section className="flex flex-col gap-3">
+                    <h1 className="text-2xl font-semibold">Work Experience</h1>
+                    {work.map((value, index) => (
+                        <div key={index}>
+                            <ExperienceSection experience={value} />
+                        </div>
+                    ))}
+                </section>
+            </div>
+        </main>
+    );
+};
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export default IndexPage;
+
+export const Head: HeadFC = () => <title>Home Page</title>;
---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+export type Experience = {
+    location: string;
+    position: string | null;
+    startMonth: string;
+    endMonth: string;
+    description: string[];
+};
