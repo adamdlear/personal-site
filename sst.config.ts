76c11549a2d0a16f81d5ace9876dc031526a8a6c
Adam Lear <adamlear12@gmail.com>
2025-01-11 15:14:30 -0600

    use sst to help deploy

---
+++ sst.config.ts
@@ -0,0 +1,25 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+    app(input) {
+        return {
+            name: "personal-site",
+            removal: input?.stage === "production" ? "retain" : "remove",
+            protect: ["production"].includes(input?.stage),
+            home: "aws",
+        };
+    },
+    async run() {
+        new sst.aws.StaticSite("PersonalSite", {
+            errorPage: "404.html",
+            build: {
+                command: "yarn build",
+                output: "public",
+            },
+            domain: {
+                name: "adam-lear.com",
+                redirects: ["www.adam-lear.com"],
+            },
+        });
+    },
+});
