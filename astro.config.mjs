7e74ae0f5a4d6d18ab5ebd5e265a3e3574b0e073
Adam Lear <adamlear12@gmail.com>
2025-06-15 17:18:42 -0500

    fix: add astro-sst package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import aws from "astro-sst";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: aws()
+});
