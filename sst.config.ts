0481ce3a7872d7d287138a098ced8f21b7983d08
Adam Lear <adamlear12@gmail.com>
2025-06-21 17:52:35 -0500

    fix: don't recreate domain, use existing hosted zone

--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,21 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "personal-site",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.Astro("PersonalSite", {
-      domain: {
-        name: "adamdlear.com",
-        redirects: ["www.adamdlear.com"],
-      },
-    });
-  },
+    app(input) {
+        return {
+            name: "personal-site",
+            removal: input?.stage === "production" ? "retain" : "remove",
+            protect: ["production"].includes(input?.stage),
+            home: "aws",
+        };
+    },
+    async run() {
+        new sst.aws.Astro("PersonalSite", {
+            domain: {
+                name: "adamdlear.com",
+                redirects: ["www.adamdlear.com"],
+                dns: sst.aws.dns({ zone: "Z08100281IZ4D4IAXD0LA" }),
+            },
+        });
+    },
 });
